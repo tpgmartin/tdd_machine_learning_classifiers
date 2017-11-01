d8794fc290cd6235a7fcce17006dcb0f4eb232b4
Tom Martin <tom@tpgmartin.com>
2017-11-01 09:33:07 +0000

    Next tests: add question class, question attributes should match training data

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,4 +1,16 @@
 class DecisionTreeClassifier(object):
-     
-     def __init__(self):
-         self.node = {}
+
+    def __init__(self, data):
+        self.node = Node(data)
+
+
+class Node(object):
+
+    def __init__(self, data):
+        self.question = Question(data)
+
+class Question(object):
+
+    def __init__(self, data):
+        self.column = data.columns[0]
+        self.value = data[data.columns[0]][0]
--- decision_tree/test.py
+++ decision_tree/test.py
@@ -1,14 +1,50 @@
+import pandas as pd
 from main import DecisionTreeClassifier
 
 # colour and size  features, target label
-training_data = [
-    ['Green', 3, 'Apple'],
-    ['Yellow', 3, 'Apple'],
-    ['Red', 1, 'Grape'],
-    ['Red', 1, 'Grape'],
-    ['Yellow', 3, 'Lemon'],
-]
+training_data = pd.DataFrame([
+    {
+        'Colour': 'Green',
+        'Size': 3,
+        'Label': 'Apple',
+    },
+    {
+        'Colour': 'Yellow',
+        'Size': 3,
+        'Label': 'Apple',
+    },
+    {
+        'Colour': 'Red',
+        'Size': 1,
+        'Label': 'Grape',
+    },
+    {
+        'Colour': 'Red',
+        'Size': 1,
+        'Label': 'Grape',
+    },
+    {
+        'Colour': 'Yellow',
+        'Size': 3,
+        'Label': 'Lemon',
+    }
+])
 
 def test_contain_at_least_one_node():
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(training_data)
     assert clf.node != None
+
+def test_node_should_contain_question():
+    clf = DecisionTreeClassifier(training_data)
+    assert clf.node.question != None
+
+def test_node_question_should_contain_a_column_and_value():
+    clf = DecisionTreeClassifier(training_data)
+    print(clf.node)
+    assert clf.node.question.column != None
+    assert clf.node.question.value != None
+
+def test_node_question_should_contain_first_column_name_and_value_from_training_data():
+    clf = DecisionTreeClassifier(training_data)
+    assert clf.node.question.column == training_data.columns[0]
+    assert clf.node.question.value == training_data[training_data.columns[0]][0]
