33f8ad2baa2a5413b7f2c2e5e5dc44e1a08762bc
Tom Martin <tom@tpgmartin.com>
2017-11-02 23:01:44 +0000

    Handle string and int question values, move requirements file

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -7,10 +7,17 @@ class DecisionTreeClassifier(object):
 class Node(object):
 
     def __init__(self, data):
-        self.question = Question(data)
+        self.question = Question(data.columns[0], data['Colour'][0])
 
 class Question(object):
 
-    def __init__(self, data):
-        self.column = data.columns[0]
-        self.value = data[data.columns[0]][0]
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, row):
+        row_value = row[self.column]
+        if isinstance(row_value, int):
+            return row_value >= self.value
+        else:
+            return row_value == self.value
--- decision_tree/test.py
+++ decision_tree/test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from main import DecisionTreeClassifier
+from main import DecisionTreeClassifier, Question
 
 # colour and size  features, target label
 training_data = pd.DataFrame([
@@ -40,11 +40,22 @@ def test_node_should_contain_question():
 
 def test_node_question_should_contain_a_column_and_value():
     clf = DecisionTreeClassifier(training_data)
-    print(clf.node)
     assert clf.node.question.column != None
     assert clf.node.question.value != None
 
-def test_node_question_should_contain_first_column_name_and_value_from_training_data():
+def test_question_should_contain_first_column_name_and_value_from_training_data():
     clf = DecisionTreeClassifier(training_data)
-    assert clf.node.question.column == training_data.columns[0]
-    assert clf.node.question.value == training_data[training_data.columns[0]][0]
+    # Pass in column by (string) name
+    assert clf.node.question.column == 'Colour'
+    # Get values for given column e.g. training_data['Colour']
+    assert clf.node.question.value == training_data['Colour'][0]
+
+def test_question_return_match_against_question_instance_for_string_values():
+    question = Question('Colour', 'Green')
+    assert question.match(training_data.iloc[0]) == True
+    assert question.match(training_data.iloc[1]) == False
+
+def test_question_return_match_against_question_instance_for_numeric_values():
+    question = Question('Size', 3)
+    assert question.match(training_data.iloc[0]) == True
+    assert question.match(training_data.iloc[2]) == False
