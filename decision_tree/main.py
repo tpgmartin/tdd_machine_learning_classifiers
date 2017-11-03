b69af153a489552971f900605ea5a9bd0fd9f4b5
Tom Martin <tom@tpgmartin.com>
2017-11-03 17:16:36 +0000

    Add Leaf class, add method to Node class, other refactoring

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,13 +1,33 @@
+import pandas as pd
+
 class DecisionTreeClassifier(object):
 
     def __init__(self, data):
         self.node = Node(data)
+
+class Leaf((object)):
+
+    def __init__(self, data):
+        self.predictions = self.class_counts(data)
+
+    def class_counts(self, data):
+        return data['Label'].value_counts()
+
 
 
 class Node(object):
 
     def __init__(self, data):
         self.question = Question(data.columns[0], data['Colour'][0])
+
+    def partition(self, data, question):
+        true_rows, false_rows = [], []
+        for _, row in data.iterrows():
+            if question.match(row):
+                true_rows.append(row)
+            else:
+                false_rows.append(row)
+        return pd.DataFrame(true_rows), pd.DataFrame(false_rows)
 
 class Question(object):
 
@@ -16,7 +36,7 @@ class Question(object):
         self.value = value
 
     def match(self, row):
-        row_value = row[self.column]
+        row_value = row.get_value(self.column)
         if isinstance(row_value, int):
             return row_value >= self.value
         else:
--- decision_tree/test.py
+++ decision_tree/test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from main import DecisionTreeClassifier, Question
+from main import DecisionTreeClassifier, Leaf, Node, Question
 
 # colour and size  features, target label
 training_data = pd.DataFrame([
@@ -30,25 +30,40 @@ training_data = pd.DataFrame([
     }
 ])
 
+# DecisionTreeClassifier class
 def test_contain_at_least_one_node():
     clf = DecisionTreeClassifier(training_data)
     assert clf.node != None
 
-def test_node_should_contain_question():
-    clf = DecisionTreeClassifier(training_data)
-    assert clf.node.question != None
+# Leaf class
+def test_leaf_should_contain_dictionary_of_labels_with_frequency():
+    leaf = Leaf(training_data)
+    assert leaf.predictions.equals(training_data['Label'].value_counts())
 
+# Node class
+def test_node_should_contain_question():
+    node = Node(training_data)
+    assert node.question != None
+
+# Use multiple test cases
+def test_node_should_partition_data_set_by_question():
+    node = Node(training_data)
+    question = Question('Colour', 'Green')
+    true_rows, false_rows = node.partition(training_data, question)
+    assert true_rows.equals(training_data.loc[training_data['Colour'] == 'Green'])
+    assert false_rows.equals(training_data.drop([0]))
+
+# Question class
 def test_node_question_should_contain_a_column_and_value():
-    clf = DecisionTreeClassifier(training_data)
-    assert clf.node.question.column != None
-    assert clf.node.question.value != None
+    node = Node(training_data)
+    assert node.question.column != None
+    assert node.question.value != None
 
 def test_question_should_contain_first_column_name_and_value_from_training_data():
-    clf = DecisionTreeClassifier(training_data)
     # Pass in column by (string) name
-    assert clf.node.question.column == 'Colour'
-    # Get values for given column e.g. training_data['Colour']
-    assert clf.node.question.value == training_data['Colour'][0]
+    node = Node(training_data)
+    assert node.question.column == 'Colour'
+    assert node.question.value == training_data['Colour'][0]
 
 def test_question_return_match_against_question_instance_for_string_values():
     question = Question('Colour', 'Green')
